307c03bb1f2a4a878492751675495fd8d3f790ac
Alexandru Cretu <alexandru.cretu@n-able.com>
2024-03-25 21:45:13 +0200

    * removed some hardcode

--- main.go
+++ main.go
@@ -25,15 +25,15 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	// Use r.Method to check whether the request is using POST or not.
-	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
 		// If it's not, use the w.WriteHeader() method to send a 405 status
 		// code and the w.Write() method to write a "Method Not Allowed"
 		// response body. We then return from the function so that the
 		//// subsequent code is not executed.
 		// Use the http.Error() function to send a 405 status code and "Method Not
 		// Allowed" string as the response body.
-		http.Error(w, "Method Not Allowed", 405)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
